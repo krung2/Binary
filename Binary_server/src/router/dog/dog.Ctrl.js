860b6e1acf56ccc151b2c1fa46576e6e23464b7a
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-09-23 15:53:53 +0900

    diet

--- Binary_server/src/router/dog/dog.Ctrl.js
+++ Binary_server/src/router/dog/dog.Ctrl.js
@@ -1,68 +1,4 @@
 const models = require('../../model');
-const sequelize = require("sequelize");
-const Op = sequelize.Op;
-
-exports.getDog = async (req, res) => {
-    try {
-        const dog = await models.Dog.findOne ({
-            where: {
-                idx: req.params.idx,
-            },
-            raw: true,
-        });
-
-        res.status(200).json ({
-            data: {
-                dog,
-            },
-        });
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({
-            message: "서버 오류",
-        });
-    }
-}
-
-exports.getDogs = async (req, res) => {
-    try {
-        let dogs = []
-        if (req.query.keyword) {
-            dogs = await models.Dog.findAll ({
-                where: {
-                    title: {
-                        [Op.like]: "%" + req.query.keyword + "%"
-                    }
-                },
-                order: [
-                    ['createdAt', 'DESC'],
-                ],
-                raw: true,
-            });
-        } else {
-            dogs = await models.dog.findAll ({
-                order: [
-                    ['createdAt', 'DESC'],
-                ],
-                raw: true,
-            });
-        }
-
-        return res.status(200).json ({
-            message: '조회 성공!',
-            data: {
-                dogs,
-            }
-        });
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({
-            message: "서버 오류",
-        });
-    }
-}
 
 exports.postDog = async (req, res) => {
     const { body } = req;
@@ -79,34 +15,16 @@ exports.postDog = async (req, res) => {
         console.log("정보 넣기 성공!");
         return res.status(200).json({
             message: "정보 넣기 성공!",
+            data: {
+                dogName: body.dogName,
+                breed: body.breed,
+                bloodType: body.bloodType,
+            }
         });
     } catch (err) {
         console.log(err)
         return res.status(500).json({
             message: "서버 오류",
         })
-    }
-}
-
-exports.modifyDog = async (req, res) => {
-    const { body } = req;
-    const { idx } = req.params;
-
-    try {
-        await models.Dog.update (body, {
-            where: {
-                idx,
-            }
-        });
-
-        return res.status(200).json ({
-            message: "정보 수정 성공!",
-        });
-
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json ({
-            message: "서버 오류",
-        });
     }
 }
--- Binary_server/src/router/dog/index.js
+++ Binary_server/src/router/dog/index.js
@@ -2,9 +2,5 @@ const router = require('express').Router();
 const dogCtrl = require('./dog.Ctrl');
 
 router.post('/', dogCtrl.postDog);
-router.get('/', dogCtrl.getDog);
-
-router.get('/:idx', dogCtrl.getDogs);
-router.put('/:idx', dogCtrl.modifyDog);
 
 module.exports = router;
