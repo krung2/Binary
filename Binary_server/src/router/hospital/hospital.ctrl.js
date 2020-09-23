61799cba86ba6d9999fc7411fefef17ae0798f4b
jungbin0722 <jungbin4337@dgsw.hs.kr>
2020-09-23 18:11:57 +0900

    make hospital find

---
+++ Binary_server/src/router/hospital/hospital.ctrl.js
@@ -0,0 +1,37 @@
+const models = require('../../model');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
+
+exports.findHospital = async (req, res) => {
+    try {
+        let hospitals = []
+        if (req.query.keyword) {
+            hospitals = await models.Hospital.findAll ({
+                where: {
+                    hospitalName: {
+                        [Op.like]: "%" + req.query.keyword + "%"
+                    }
+                },
+                raw: true,
+            });
+        } else {
+            hospitals = await models.Hospital.findAll ({
+                where: {
+                    attributes: ['hospitalName'],Name
+                }
+            });
+        }
+
+        return res.status(200).json({
+            message: "조회 성공",
+            data: {
+                hospitals,
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: "서버 오류",
+        });
+    }
+}
--- Binary_server/src/router/hospital/index.js
+++ Binary_server/src/router/hospital/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const hospitalCtrl = require('./hospital.ctrl');
 
-// router.get('/', hospitalCtrl.findHospital);
+router.get('/', hospitalCtrl.findHospital);
 
 module.exports = router;
